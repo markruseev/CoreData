fdb8a9f007d4267db017b364a08c2f5b04c401fc
Марк Русеев <markruseev@marks-macbook-pro.local>
2019-03-03 14:31:09 +0500

    Initial Commit

---
+++ CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Марк Русеев on 03/03/2019.
+//  Copyright © 2019 Mark Ruseev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
