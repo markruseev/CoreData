4637fc5a100788c9c8ced73ca4162416433834f7
Марк Русеев <markruseev@marks-macbook-pro.local>
2019-03-05 15:45:38 +0500

    Convert to MVC design pattern

---
+++ CoreData/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  CoreData
+//
+//  Created by Марк Русеев on 05/03/2019.
+//  Copyright © 2019 Mark Ruseev. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
